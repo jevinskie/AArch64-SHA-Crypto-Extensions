49d02b7cba0b01ae9a9d3025fed67e015147401c
Jevin Sweval <jevinsweval@gmail.com>
2024-12-11 14:39:50 -0500

    wip

--- uint32_to_ascii_hex.cpp
+++ uint32_to_ascii_hex.cpp
@@ -28,19 +28,38 @@ uint32_t nibbles_to_hex_swar(uint32_t nibbles) {
 }
 
 [[gnu::noinline]] void u32_to_hex(const uint32_t n, char (&s)[9]) {
+#if 0
     for (int i = 0; i < 8; ++i) {
         s[i] = 'a' + i;
     }
+#endif
+#if 0
     const uint32_t nlo = nibbles_to_hex_swar(n & 0x0f0f'0f0fu);
     std::memcpy(s, &nlo, sizeof(nlo));
+#endif
+#if 1
     const uint32_t nhi = nibbles_to_hex_swar((n >> 4) & 0x0f0f'0f0fu);
     std::memcpy(s, &nhi, sizeof(nhi));
+#endif
+    const uint64_t foo = nhi * 0x0000'0000'0101'0101llu;
+    printf("foo:  0x%016llx\n", foo);
+    std::memset(s, 'x', sizeof(foo));
+    // const uint64_t mask = 0x00ff'00ff'00ff'00ffllu;
+    const uint64_t mask = 0xff00'ff00'ff00'ff00llu;
+    printf("mask: 0x%016llx\n", mask);
+    const uint64_t sx = *(uint64_t *)&s & mask;
+    printf("sx:   0x%016llx\n", sx);
+    // const uint64_t sx = *(uint64_t *)&s;
+    const uint64_t bar = foo | sx;
+    // const uint64_t bar = sx;
+    printf("bar:  0x%016llx\n", bar);
+    std::memcpy(s, &bar, sizeof(bar));
     s[sizeof(s) - 1] = 0;
 }
 
 int main(void) {
     const uint32_t n = 0xDEAD'BEEFu;
-    char s[9];
+    char s[9]        = {};
     u32_to_hex(n, s);
     printf("n: 0x%08x s: '%s'\n", n, s);
     return 0;
