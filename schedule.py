27bf0d60388c85a9b2bfe6a966ca08dcdb5d344e
Jevin Sweval <jevinsweval@gmail.com>
2024-12-21 23:42:14 -0500

    broken wip

--- schedule.py
+++ schedule.py
@@ -8,11 +8,16 @@ from collections.abc import Mapping, MutableMapping
 
 import bidict
 import networkx as nx
+import pandas as pd
+import tabulate
 from ortools.sat.python import cp_model
 from rich import print as rprint
 from rich.pretty import pprint
 
 from sha1_arm import cf, op_color
+
+tabulate
+pd
 
 
 class BiDictStrInt(Mapping[str, int]):
@@ -279,7 +284,7 @@ exec_units = {
 ticks = [f"t{i}" for i in range(22)]
 
 # List of possible operations
-operations = ["sha1c", "sha1h", "sha1m", "sha1p", "sha1su0", "sha1su1", "vaddX", "vaddXY", "vaddY"]
+operations = ("sha1c", "sha1h", "sha1m", "sha1p", "sha1su0", "sha1su1", "vaddX", "vaddXY", "vaddY")
 add_operations = {"vaddX", "vaddY", "vaddXY"}
 rprint(add_operations)
 
@@ -338,8 +343,20 @@ print(f"  - wall time: {solver.wall_time}s")
 print(solver.solution_info())
 print(model.model_stats())
 
+solved_schedule = collections.defaultdict(lambda: collections.defaultdict(collections.defaultdict))
+for e in exec_units:
+    for o in operations:
+        for t in ticks:
+            solved_schedule[e][o][t] = not not solver.value(schedule[e][o][t])
+
+rprint(solved_schedule)
+
+pdf = pd.DataFrame(schedule)
+rprint(pdf.describe())
+rprint(pdf)
+
 # Print the solution
-print(f"{' '*10} | " + " | ".join([f"{t:^9}" for t in ticks]) + " | Total |")
+print(f"{' '*3} | " + " | ".join([f"{t:^3}" for t in ticks]) + " | Total |")
 
 print(f"{' '*10} | " + " | ".join(["M | A | E" for t in range(len(ticks))]) + " |       |")
 
@@ -466,11 +483,11 @@ if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
 else:
     print("No solution found.")
 
-    # Statistics.
-    print("\nStatistics")
-    print(f"  - conflicts: {solver.num_conflicts}")
-    print(f"  - branches : {solver.num_branches}")
-    print(f"  - wall time: {solver.wall_time}s")
+# Statistics.
+print("\nStatistics")
+print(f"  - conflicts: {solver.num_conflicts}")
+print(f"  - branches : {solver.num_branches}")
+print(f"  - wall time: {solver.wall_time}s")
 
 
 rprint("done")
